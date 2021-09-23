f989d4d1230fbffc6e0b819585e625aeb90223e1
nikitacph <90254110+nikitacph@users.noreply.github.com>
2021-09-23 22:14:40 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+/*console.log ("Hallo!")
+
+var hamburgerButton = document.getElementsByClassName("hamburgermenu")
+var menuGeopend = document.getElementsByClassName("open")
+var sluitButton = document.getElementsByClassName("sluit")
+var logoBijenkorf = document.getElementsByClassName("logoBijenkorf")
+
+function openMenu() {
+    hamburgerButton.classList.toggle("open");
+}
+
+function sluitMenu() {
+sluitButton.classList.toggle("sluit");
+}
+
+hamburgerButton.addEventListener("click", openMenu);
+
+sluitButton.addEventListener("click", sluitMenu);
+
+logoBijenkorf.addEventListener("click", sluitMenu);
+*/
