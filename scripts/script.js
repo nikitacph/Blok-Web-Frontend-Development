545b9779180b81d7cc7bcda580e3dc4795c4894f
nikitacph <90254110+nikitacph@users.noreply.github.com>
2021-10-04 13:36:47 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ console.log("Hallo!");
 const hamburgerButton = document.getElementsByClassName("hamburgerButton")[0];
 const menuGeopend = document.getElementsByClassName("open")[0];
 const sluitButton = document.getElementsByClassName("sluit")[0];
-const logoBijenkorf = document.getElementsByClassName("logoBijenkorf")[0];
+const logoBijenkorf = document.getElementsByClassName("logo_Bijenkorf")[0];
 
 function openMenu() {
      menuGeopend.classList.add("showMenu");
